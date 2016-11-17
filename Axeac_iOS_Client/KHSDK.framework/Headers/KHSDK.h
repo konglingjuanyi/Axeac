fe1b6f376bd8d508a7085c38673b509685344365
dongxiaowei <dotaman@sina.cn>
2016-11-17 09:28:09 +0800

    iOS SDK first commit

---
+++ Axeac_iOS_Client/KHSDK.framework/Headers/KHSDK.h
@@ -0,0 +1,18 @@
+//
+//  KHMOP.h
+//  KHMOP
+//
+//  Created by dnnta on 2016/11/16.
+//  Copyright © 2016年 axeac. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KHSDK : NSObject
+
++ (NSString *)sdkVersion;
+
++ (UIViewController *)workCenterVC;
+
+@end
+
